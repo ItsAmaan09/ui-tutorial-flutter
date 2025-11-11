e0d4b50f2b1712cd2acfc10bb81d5a5b29660019
mohammed amaan <amaanvisiontech@gmail.com>
2025-11-11 15:25:56 +0530

    signup screen update

--- lib/screens/Signup-Screen.dart
+++ lib/screens/Signup-Screen.dart
@@ -1,38 +1,78 @@
 import 'package:flutter/material.dart';
-import 'package:tutorial/pages/Main-Page.dart';
-import 'package:tutorial/screens/Signup-Screen.dart';
+import 'package:tutorial/screens/Login-Screen-Update.dart';
 
-class LoginScreenUpdate extends StatefulWidget {
-  const LoginScreenUpdate({super.key});
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({super.key});
 
   @override
-  State<LoginScreenUpdate> createState() => _LoginScreenState();
+  State<SignupScreen> createState() => _SignupScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreenUpdate> {
+class _SignupScreenState extends State<SignupScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        title: Text('Create Account'),
+      ),
       body: SingleChildScrollView(
         padding: EdgeInsets.all(24.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            SizedBox(height: 16),
-            Icon(Icons.login_outlined, size: 58),
-            SizedBox(height: 8),
             Text(
-              "Welcome",
+              "Create Account",
               style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.left,
             ),
             SizedBox(height: 8),
-            Text(
-              "Enter your email and password to sign in",
-              style: TextStyle(fontSize: 16, color: Colors.grey),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  "Enter your Name, Email and Password for signup.",
+                  style: TextStyle(fontSize: 16, color: Colors.grey),
+                ),
+                SizedBox(height: 8),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      "Already have an account? ",
+                      style: TextStyle(fontSize: 16, color: Colors.grey),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => LoginScreenUpdate(),
+                          ),
+                        );
+                      },
+                      child: Text(
+                        "Login",
+                        style: TextStyle(fontSize: 16, color: Colors.blue),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
+
             SizedBox(height: 24),
+            TextField(
+              decoration: InputDecoration(
+                labelText: "Full Name",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
             TextField(
               decoration: InputDecoration(
                 labelText: "Email address",
@@ -54,17 +94,9 @@ class _LoginScreenState extends State<LoginScreenUpdate> {
                     : Icon(Icons.visibility),
               ),
             ),
-            SizedBox(height: 5),
-            Text(
-              "Forget Password?",
-              style: TextStyle(color: Colors.grey),
-              textAlign: TextAlign.right,
-            ),
             SizedBox(height: 16),
             ElevatedButton(
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => MainPage()));
-              },
+              onPressed: () {},
               style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.green,
                 shape: RoundedRectangleBorder(
@@ -72,27 +104,7 @@ class _LoginScreenState extends State<LoginScreenUpdate> {
                 ),
                 minimumSize: Size(double.infinity, 48),
               ),
-              child: Text('SIGN IN', style: TextStyle(color: Colors.white)),
-            ),
-            SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("New User?"),
-                SizedBox(width: 5),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => SignupScreen()),
-                    );
-                  },
-                  child: Text(
-                    "Sign Up",
-                    style: TextStyle(color: Colors.blue),
-                  ),
-                ),
-              ],
+              child: Text('SIGN UP', style: TextStyle(color: Colors.white)),
             ),
             SizedBox(height: 16),
             Row(
